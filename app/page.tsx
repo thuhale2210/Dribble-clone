67d6873c6f6fa6ec34f39f724d56b009596f30b6
thuhale2210 <thuhale2210@gmail.com>
2023-08-17 12:37:49 -0400

    Update pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { ProjectInterface } from '@/common.types'
+import Categories from '@/components/Categories';
+import LoadMore from '@/components/LoadMore';
 import ProjectCard from '@/components/ProjectCard';
 import ProjectForm from '@/components/ProjectForm';
 import { fetchAllProjects } from '@/lib/actions'
+// import { Props } from '@headlessui/react/dist/types';
 import React from 'react'
 
 type ProjectSearch = {
@@ -16,27 +19,42 @@ type ProjectSearch = {
   },
 }
 
-const Home = async () => {
-  const data = await fetchAllProjects() as ProjectSearch
+type SearchParams = {
+  category?: string
+  endcursor?: string
+}
+
+type Props = {
+  searchParams: SearchParams
+}
+
+export const dynamic = 'force-dynamic' // to force the content to change
+export const dynamicParams = true
+export const revalidate = 0
+
+const Home = async ({ searchParams: { category, endcursor } }: Props) => {
+  const data = await fetchAllProjects(category, endcursor) as ProjectSearch
 
   const projectsToDisplay = data?.projectSearch?.edges || []
 
   if (projectsToDisplay.length === 0) {
     return (
       <section className='flexStart flex-col paddings'>
-        Categories
+        <Categories />
 
         <p className='no-result-text text-center'>No projects found</p>
       </section>
     )
   }
 
+  const pagination = data?.projectSearch?.pageInfo
+
   return (
     <section className='flex-start flex-col paddings mb-16'>
-      <h1>Categories</h1>
+      <Categories />
 
       <section className='projects-grid'>
-        {projectsToDisplay.map(({ node}: {node:ProjectInterface}) => (
+        {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
           <ProjectCard
             key={node?.id}
             id={node?.id}
@@ -45,11 +63,15 @@ const Home = async () => {
             name={node?.createdBy?.name}
             avatarUrl={node?.createdBy?.avatarUrl}
             userId={node?.createdBy?.id}
-             />
+          />
         ))}
       </section>
 
-      <h1>LoadMore</h1>
+      <LoadMore
+        startCursor={pagination.startCursor}
+        endCursor={pagination.endCursor}
+        hasPreviousPage={pagination.hasPreviousPage}
+        hasNextPage={pagination.hasNextPage} />
     </section>
   )
 }
