96a1d3f267319153acb6aa58054fc963d1dddeae
thuhale2210 <thuhale2210@gmail.com>
2023-08-17 12:38:29 -0400

    Update ProjectForm.tsx

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,85 +1,112 @@
-'use client'
+"use client"
 
-import { SessionInterface } from "@/common.types"
-import { ChangeEvent, useState } from "react"
-import Image from "next/image"
-import FormField from "./FormField"
-import { categoryFilters } from "@/constants"
-import CustomMenu from "./CustomMenu"
-import Button from "./Button"
-import { createNewProject, fetchToken } from "@/lib/actions"
-import { useRouter } from "next/navigation"
+import Image from 'next/image';
+import React, { ChangeEvent, FormEvent, useState } from 'react'
+import { useRouter } from 'next/navigation';
+
+import FormField from './FormField';
+import Button from './Button';
+import CustomMenu from './CustomMenu';
+import { categoryFilters } from '@/constants';
+import { updateProjects, createNewProject, fetchToken } from '@/lib/actions';
+import { FormState, ProjectInterface, SessionInterface } from '@/common.types';
 
 type Props = {
     type: string,
     session: SessionInterface,
+    project?: ProjectInterface
 }
 
-const ProjectForm = ({ type, session }: Props) => {
+const ProjectForm = ({ type, session, project }: Props) => {
     const router = useRouter()
 
-    const handleFormSubmit = async (e: React.FormEvent) => {
-        e.preventDefault()
+    const [submitting, setSubmitting] = useState<boolean>(false);
+    const [form, setForm] = useState<FormState>({
+        title: project?.title || "",
+        description: project?.description || "",
+        image: project?.image || "",
+        liveSiteUrl: project?.liveSiteUrl || "",
+        githubUrl: project?.githubUrl || "",
+        category: project?.category || ""
+    })
 
-        setIsSubmitting(true)
+    const handleStateChange = (fieldName: keyof FormState, value: string) => {
+        setForm((prevForm) => ({ ...prevForm, [fieldName]: value }));
+    };
+
+    const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+
+        const file = e.target.files?.[0];
+
+        if (!file) return;
+
+        if (!file.type.includes('image')) {
+            alert('Please upload an image!');
+
+            return;
+        }
+
+        const reader = new FileReader();
+
+        reader.readAsDataURL(file);
+
+        reader.onload = () => {
+            const result = reader.result as string;
+
+            handleStateChange("image", result)
+        };
+    };
+
+    const handleFormSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+
+        setSubmitting(true)
 
         const { token } = await fetchToken()
 
         try {
             if (type === "create") {
                 await createNewProject(form, session?.user?.id, token)
-
                 router.push("/")
+                // App doesn't load new image immediately. Have to reload page
             }
+
+            if (type === "edit") {
+                await updateProjects(form, project?.id as string, token)
+                router.push("/")
+                // App doesn't load new image immediately. Have to reload page
+            }
+
         } catch (error) {
             alert(`Failed to ${type === "create" ? "create" : "edit"} a project. Try again!`);
-            console.log(error)
         } finally {
-            setIsSubmitting(false)
+            setSubmitting(false)
         }
     }
-    const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
-        e.preventDefault()
-
-        const file = e.target.files?.[0]
-
-        if (!file) return
-        if (!file.type.includes('image')) {
-            return alert('Please upload an image file')
-        }
-        const reader = new FileReader()
-        reader.readAsDataURL(file)
-        reader.onload = () => {
-            const result = reader.result as string
-            handleStateChange('image', result)
-        }
-    }
-    const handleStateChange = (fieldName: string, value: string) => {
-        setForm((prevState) => ({ ...prevState, [fieldName]: value }))
-    }
-
-    const [isSubmitting, setIsSubmitting] = useState(false)
-    const [form, setForm] = useState({
-        title: '',
-        description: '',
-        image: '',
-        liveSiteUrl: '',
-        githubUrl: '',
-        category: ''
-    })
 
     return (
-        <form onSubmit={handleFormSubmit} className="flexStart form">
+        <form
+            onSubmit={handleFormSubmit}
+            className="flexStart form">
             <div className="flexStart form_image-container">
-                <label htmlFor='poster' className="flexCenter form_image-label">
+                <label htmlFor="poster" className="flexCenter form_image-label">
                     {!form.image && 'Choose a poster for your project'}
                 </label>
-                {/* type='file' allows us to open a dialog to upload file */}
-                <input id='image' type='file' accept='image/*' required={type === 'create'}
-                    className="form_image-input" onChange={handleChangeImage}
+                <input
+                    id="image"
+                    type="file"
+                    accept='image/*'
+                    required={type === "create" ? true : false}
+                    className="form_image-input"
+                    onChange={(e) => handleChangeImage(e)}
                 />
                 {form.image && (
-                    <Image src={form?.image} className='sm:p-10 object-contain z-20' alt='Project poster' fill />
+                    <Image
+                        src={form?.image}
+                        className="sm:p-10 object-contain z-20" alt="image"
+                        fill
+                    />
                 )}
             </div>
 
@@ -102,7 +129,7 @@ const ProjectForm = ({ type, session }: Props) => {
                 type="url"
                 title="Website URL"
                 state={form.liveSiteUrl}
-                placeholder="https://thuhale2210.github.io"
+                placeholder="https://jsmastery.pro"
                 setState={(value) => handleStateChange('liveSiteUrl', value)}
             />
 
@@ -110,7 +137,7 @@ const ProjectForm = ({ type, session }: Props) => {
                 type="url"
                 title="GitHub URL"
                 state={form.githubUrl}
-                placeholder="https://github.com/thuhale2210"
+                placeholder="https://github.com/adrianhajdin"
                 setState={(value) => handleStateChange('githubUrl', value)}
             />
 
@@ -123,10 +150,10 @@ const ProjectForm = ({ type, session }: Props) => {
 
             <div className="flexStart w-full">
                 <Button
-                    title={isSubmitting ? `${type === "create" ? "Creating" : "Editing"}` : `${type === "create" ? "Create" : "Edit"}`}
-                    type='submit'
-                    leftIcon={isSubmitting ? '' : '/plus.svg'}
-                    isSubmitting={isSubmitting}
+                    title={submitting ? `${type === "create" ? "Creating" : "Editing"}` : `${type === "create" ? "Create" : "Edit"}`}
+                    type="submit"
+                    leftIcon={submitting ? "" : "/plus.svg"}
+                    isSubmitting={submitting}
                 />
             </div>
         </form>
